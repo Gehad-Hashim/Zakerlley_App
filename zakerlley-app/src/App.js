20ae9fa830b9abc4a7531f8952b068ab84eaf0bf
Gehad Hashim <gehad7hashim@outlook.com>
2023-07-08 21:20:10 +0200

    project setup

---
+++ zakerlley-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/headerComponent/header'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>e-learning app</h1>
+    <Header/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
